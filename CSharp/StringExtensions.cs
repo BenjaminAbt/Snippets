072ec05a990bd14a55a486d865a059ce4b57d6ad
Benjamin Abt <BenjaminAbt@users.noreply.github.com>
2016-03-11 13:06:32 +0100

    Update StringExtensions.cs

--- CSharp/StringExtensions.cs
+++ CSharp/StringExtensions.cs
@@ -1,5 +1,8 @@
 public static class StringExtensions
 {
+      /// <summary>
+      /// Cuts a string with the given max length
+      /// </summary>
       public static string WithMaxLength(this string value, int maxLength)
       {
           return value?.Substring(0, Math.Min(value.Length, maxLength));
